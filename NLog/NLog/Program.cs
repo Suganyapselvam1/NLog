1a023808591a17dfe5169327d2dfb65022819edb
Suganya Panneerselvam <suganya.panneerselvam@dataquest.com.au>
2021-12-14 17:04:39 +0530

    Adding rules and writing logs

--- NLog/NLog/Program.cs
+++ NLog/NLog/Program.cs
@@ -8,8 +8,11 @@ namespace NLog
 {
     class Program
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            Logger.Info("Hello world");
+            Console.WriteLine("sugu");
         }
     }
 }
